3a3c3e9854b26e508ceef7ecfcaf07f0bc37c57b
diego <reyesdiego@hotmail.com>
2026-05-04 00:24:37 -0300

    url api

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // Vite default base URL, check import.meta.env in production
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || `http://${window.location.hostname}:8000`,
+  baseURL: import.meta.env.VITE_API_URL || `http://namai-api.diegoreyes.io`,
 });
 
 api.interceptors.request.use((config) => {
